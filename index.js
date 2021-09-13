f686295ee3540c80900d3071c3aa2e304432caaa
sovancharan <59173364+sovancharan@users.noreply.github.com>
2021-09-13 21:35:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+
+let countEl = document.getElementById("count-el")
+let saveEl=document.getElementById("save-el")
+
+let count =0;
+
+function increment() {
+
+    count=count+1;
+    countEl.innerText=count; 
+
+    console.log(count)
+}
+
+function save() {
+let countstr=count+"-"
+saveEl.innerText +=countstr
+console.log(count)
+}
